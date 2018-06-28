cce3d00b870fb2f666f3b57a2a77722f69f836c7
angelodel80 <angelodel80@gmail.com>
2018-06-28 19:54:41 +0200

    added feature for filtering and search by keyword

--- src/app.js
+++ src/app.js
@@ -5,3 +5,20 @@ var app = angular.module('OmegaRest',[]);
 require('./directives');
 require('./services');
 require('./controllers');
+
+/*$(function() {
+    var $input = $("input[name='keyword']"),
+      $context = $("#textContext");
+    $input.on("input", function() {
+      var term = $(this).val();
+      $context.show().unmark();
+      if (term) {
+        $context.mark(term, {
+          done: function() {
+            $context.not(":has(mark)").hide();
+          }
+        });
+      }
+    });
+  });
+  */
